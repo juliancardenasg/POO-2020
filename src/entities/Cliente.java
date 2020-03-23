95d4d4ba0dcf82662ea487a25b2134b1aaed45f8
Julian Cárdenas <cardenas.julian1031@gmail.com>
2020-03-22 22:21:58 -0500

    Algunos cambios de prueba

--- src/entities/Cliente.java
+++ src/entities/Cliente.java
@@ -3,32 +3,3 @@ package entities;
 public class Cliente {
 
 }
-
-/*
-Buenas tardes,
-
-Ya cree los tres diferentes espacios de trabajo (tres branches/ramas), distribuidos asi para Cliente:
-Nombre del responsable: Edgar Baquero
-
-Tener en cuenta lo siguiente:
-- Si usted va a hacer cambios tanto en la entidad (Cliente) como en el control
-(GestionCliente) solo hacer commits en esta rama: "cliente", y cuando tenga
-la version final (ultimo commit), avisar a Julian para que el haga el respectivo
-merge a la rama final (master). Haga cuantos commits desee, pero no olvide
-comentar cada uno de ellos. En resumen, haga los cambios en su repo local, hasta
-que tenga la versión final de esta rama, no avisar.
-- Comunicación por el grupo de Whatsapp para cualquier cosa.
-- Si tiene alguna duda acerca de Git o GitHub, no dude con contactarme, yo le
-ayudo a configurar su repositorio local al remoto y viceversa.
-- Tenga en cuenta el buen manejo de los commits.
-- Como mencione anteriormente, solo gestione y edite sus dos correspondientes
-archivos, despues todos nos encargamos de fusionar y hacer los respectivos
-cambios.
-- Fecha de entrega de esta rama (cliente): Viernes 27 de Marzo
-- No borrar estos comentarios
-
-
-Julian,
-
-
-*/
