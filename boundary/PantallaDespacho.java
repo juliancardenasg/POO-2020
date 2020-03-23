83e2c606b8e2f8b56ced77a21c631630b822b7d9
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-23 12:41:59 -0500

    Agregue el primer cambio en la rama de desarrollo de cliente, rama de Edgar

--- boundary/PantallaDespacho.java
+++ boundary/PantallaDespacho.java
@@ -3,3 +3,13 @@ package boundary;
 public class PantallaDespacho {
 
 }
+
+/*
+Buenas tardes a todos,
+
+Por favor nadie toque este espacio en su rama, este espacio solo es
+modificable en la rama master, es decir, la rama final de todo el proyecto.
+
+Julian,
+
+*/
