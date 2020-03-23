238bcbca9626997420268b6dbedefce3ad4f5314
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-22 22:10:15 -0500

    C
    cA

---
+++ src/boundary/PantallaDespacho.java
@@ -0,0 +1,11 @@
+package boundary;
+
+public class PantallaDespacho {
+
+}
+
+/*
+POR FAVOR NADIE EDITE O MODIFIQUE ESTE ESPACIO HASTA AVISO
+
+SALUDOS
+*/
---
+++ src/entities/ServicioAdicional.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class ServicioAdicional {
+
+}
+
+/*
+De esta entidad, se encarga el que le sobre tiempo en el proyecto.
+
+Antes de editar, por favor avisar.
+ 
+*/
