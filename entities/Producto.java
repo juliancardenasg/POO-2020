93cb714300a0baaf35f69fd5abc4850e130c537c
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-23 12:58:13 -0500

    Agregue el primer cambio en la rama de desarrollo de producto, rama de Ricardo T

--- entities/Producto.java
+++ entities/Producto.java
@@ -3,3 +3,32 @@ package entities;
 public class Producto {
 
 }
+
+/*
+Buenas tardes,
+
+Ya cree los tres diferentes espacios de trabajo (tres branches/ramas), distribuidos asi para Producto:
+Nombre del responsable: Ricardo Sebastian T
+
+Tener en cuenta lo siguiente:
+- Si usted va a hacer cambios tanto en la entidad (Producto) como en el control
+(GestionProducto) solo hacer commits en esta rama: "producto", y cuando tenga
+la version final (ultimo commit), avisar a Julian para que el haga el respectivo
+merge a la rama final (master). Haga cuantos commits desee, pero no olvide
+comentar cada uno de ellos. En resumen, haga los cambios en su repo local, hasta
+que tenga la versión final de esta rama, no avisar.
+- Comunicación por el grupo de Whatsapp para cualquier cosa.
+- Si tiene alguna duda acerca de Git o GitHub, no dude con contactarme, yo le
+ayudo a configurar su repositorio local al remoto y viceversa.
+- Tenga en cuenta el buen manejo de los commits.
+- Como mencione anteriormente, solo gestione y edite sus dos correspondientes
+archivos, despues todos nos encargamos de fusionar y hacer los respectivos
+cambios.
+- Fecha de entrega de esta rama (producto): Viernes 27 de Marzo
+- No borrar estos comentarios
+
+
+Julian,
+
+
+*/
