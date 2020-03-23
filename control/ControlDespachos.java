9e29b5e23d4e075bbf6849aad2887eaafbe6ec80
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-23 13:01:02 -0500

    Agregue el primer cambio a la rama de desarrollo de pedido, rama de Julian y Richard

--- control/ControlDespachos.java
+++ control/ControlDespachos.java
@@ -3,3 +3,32 @@ package control;
 public class ControlDespachos {
 
 }
+
+/*
+Buenas tardes,
+
+Ya cree los tres diferentes espacios de trabajo (tres branches/ramas), distribuidos asi para Pedido:
+Nombre del responsable: Julian Cárdenas y Richard Fonseca
+
+Tener en cuenta lo siguiente:
+- Si usted va a hacer cambios tanto en la entidad (Pedido) como en el control
+(GestionPedido) solo hacer commits en esta rama: "pedido", y cuando tenga
+la version final (ultimo commit), avisar a Julian para que el haga el respectivo
+merge a la rama final (master). Haga cuantos commits desee, pero no olvide
+comentar cada uno de ellos. En resumen, haga los cambios en su repo local, hasta
+que tenga la versión final de esta rama, no avisar.
+- Comunicación por el grupo de Whatsapp para cualquier cosa.
+- Si tiene alguna duda acerca de Git o GitHub, no dude con contactarme, yo le
+ayudo a configurar su repositorio local al remoto y viceversa.
+- Tenga en cuenta el buen manejo de los commits.
+- Como mencione anteriormente, solo gestione y edite sus dos correspondientes
+archivos, despues todos nos encargamos de fusionar y hacer los respectivos
+cambios.
+- Fecha de entrega de esta rama (pedido): Viernes 27 de Marzo
+- No borrar estos comentarios
+
+
+Julian,
+
+
+*/
