3bdaad8cf6d889585e89cd7c72c2f95f469ab90d
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-26 22:58:53 -0500

    Inicialice los metodos de cada clase

--- control/ControlDespachos.java
+++ control/ControlDespachos.java
@@ -1,11 +1,181 @@
 package control;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.util.Random;
+import entities.Cliente;
+import entities.Producto;
+import entities.Pedido;
 
 public class ControlDespachos {
-    private GestionCliente gestionCliente;
-    private GestionProducto gestionProducto;
-    private ArrayList<Cliente> listaClientes; 
-    private ArrayList<Producto> listaProductos;
-    private ArrayList<Pedido> pedidos;
+
+  private GestionCliente gestionCliente;
+	 private GestionProducto gestionProducto;
+	 private ArrayList<Cliente> listaClientes;
+	 private ArrayList<Producto> listaProductos;
+	 private ArrayList<Pedido> pedidos;
+
+
+	public ControlDespachos() {
+		super();
+	}
+
+
+	public ControlDespachos(GestionCliente gestionCliente, GestionProducto gestionProducto,
+			ArrayList<Cliente> listaClientes, ArrayList<Producto> listaProductos, ArrayList<Pedido> pedidos) {
+		super();
+		this.gestionCliente = gestionCliente;
+		this.gestionProducto = gestionProducto;
+		this.listaClientes = listaClientes;
+		this.listaProductos = listaProductos;
+		this.pedidos = pedidos;
+	}
+
+
+	public GestionCliente getGestionCliente() {
+		return gestionCliente;
+	}
+
+
+	public void setGestionCliente(GestionCliente gestionCliente) {
+		this.gestionCliente = gestionCliente;
+	}
+
+
+	public GestionProducto getGestionProducto() {
+		return gestionProducto;
+	}
+
+
+	public void setGestionProducto(GestionProducto gestionProducto) {
+		this.gestionProducto = gestionProducto;
+	}
+
+
+	public ArrayList<Cliente> getListaClientes() {
+		return listaClientes;
+	}
+
+
+	public void setListaClientes(ArrayList<Cliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+
+
+	public ArrayList<Producto> getListaProductos() {
+		return listaProductos;
+	}
+
+
+	public void setListaProductos(ArrayList<Producto> listaProductos) {
+		this.listaProductos = listaProductos;
+	}
+
+
+	public ArrayList<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+	public void setPedidos(ArrayList<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+  public boolean reservarPedido(long cedula, long pid, Producto producto){
+		if(autoGenerarPedido(cedula, pid) == true) {
+			//PUNTO C ACA
+			if(validacionPedido(cedula, pid) == false) {
+				System.out.println("Nueva fecha de pedido");
+			}
+			else {
+				System.out.println("El costo total de su pedido es ");
+				System.out.println(costoPedido(cedula, pid, producto));
+			}
+			}
+
+
+
+	   	return false;
+	}
+
+	public boolean existePedido(long numeroPedido){
+		for(Pedido aux: this.pedidos) {
+			if(aux.getNumeroPedido() == numeroPedido) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	public boolean autoGenerarPedido(long cedula, long pid) {	//Funcion que asigna un numero pedido aleatorio a un pedido que esta por reservar
+		int upperbound = 9999;									//Numero de pedido de cuatro digitos
+		Random aleatorio = new Random();
+
+	   	for(Cliente aux: this.listaClientes) {
+	   		for(Producto aux2 : this.listaProductos) {
+	   			if(cedula == aux.getCedula() && pid == aux2.getPid()) {		//Se comprueba que existe cliente y producto
+	   				long long_random = (long)aleatorio.nextInt(upperbound);
+	   					if(existePedido(long_random) == false){				//Se comprueba que el nª de pedido no haya sido asignado
+	   						return true;
+	   					}
+	   			}
+	   		}
+	   	}
+
+	   	return false;
+	}
+
+	public boolean validacionPedido(long cedula, long pid) {
+
+
+
+		return false;
+	}
+
+	public void solicitarServiciosAdicionales (long cedula, long pid) {
+		System.out.println("Bienvenido al menu: Servicios Adicionales");
+
+
+	}
+
+	public double costoPedido (long cedula, long pid, Producto producto) {
+		double costo = 0;
+		costo = producto.getPrecio + producto.getIva;
+		if(producto.getIva > 50000) {
+			costo = costo + 8000;
+		}
+
+		return costo;
+	}
+
+	public void mostrarPedido (long cedula, long pid, Producto producto, Pedido pedido) {
+
+		if(pedido.getPagado == true) {
+		System.out.println("Confirmación de su pedido");
+		System.out.println("Nª de pedido: " + pedido.getNropedido);
+		System.out.println("Producto: " + producto.getNombre);
+		System.out.println("Precio: " + producto.getPrecio);
+		System.out.println("Precio (IVA): " + producto.getIva);
+		System.out.println("Tienda: " + producto.getTienda);
+		System.out.println("Repartidor: " + pedido.getRepartidor);
+		System.out.println("Fecha de recibido: " + pedido.getFecharecibido);
+		}
+		else {
+			System.out.println("Por favor haga el pago de su pedido para hacer la confirmacion de su pedido");
+		}
+
+	}
+
+
+	public int listadoPedidos (long pid, Calendar fecha) {
+
+
+
+		return 0;
+	}
+
+
 
 }
 
--- entities/Pedido.java
+++ entities/Pedido.java
@@ -1,13 +1,80 @@
 package entities;
 import java.util.Calendar;
+import java.util.Scanner;
 
 public class Pedido {
-    private long numeroPedido;
+
+  private long numeroPedido;
     private Calendar fechaRecibido;
     private boolean pagado;
     private String nombreRepartidor;
     private Cliente solicitante;
     private Producto productoSolicitado;
+
+
+    public Pedido() {
+		super();
+	}
+
+    public Pedido(long numeroPedido, Calendar fechaRecibido, boolean pagado, String nombreRepartidor,
+			Cliente solicitante, Producto productoSolicitado) {
+		super();
+		this.numeroPedido = numeroPedido;
+		this.fechaRecibido = fechaRecibido;
+		this.pagado = pagado;
+		this.nombreRepartidor = nombreRepartidor;
+		this.solicitante = solicitante;
+		this.productoSolicitado = productoSolicitado;
+	}
+
+
+	public long getNumeroPedido() {
+		return numeroPedido;
+	}
+
+	public void setNumeroPedido(long numeroPedido) {
+		this.numeroPedido = numeroPedido;
+	}
+
+	public Calendar getFechaRecibido() {
+		return fechaRecibido;
+	}
+
+	public void setFechaRecibido(Calendar fechaRecibido) {
+		this.fechaRecibido = fechaRecibido;
+	}
+
+	public boolean isPagado() {
+		return pagado;
+	}
+
+	public void setPagado(boolean pagado) {
+		this.pagado = pagado;
+	}
+
+	public String getNombreRepartidor() {
+		return nombreRepartidor;
+	}
+
+	public void setNombreRepartidor(String nombreRepartidor) {
+		this.nombreRepartidor = nombreRepartidor;
+	}
+
+	public Cliente getSolicitante() {
+		return solicitante;
+	}
+
+	public void setSolicitante(Cliente solicitante) {
+		this.solicitante = solicitante;
+	}
+
+	public Producto getProductoSolicitado() {
+		return productoSolicitado;
+	}
+
+	public void setProductoSolicitado(Producto productoSolicitado) {
+		this.productoSolicitado = productoSolicitado;
+	}
 
 }
 
