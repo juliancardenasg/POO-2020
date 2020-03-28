fa3e0fcc9ed71fe808a5288349b55eb09f1e8a37
ed4st <61949882+ed4st@users.noreply.github.com>
2020-03-28 12:24:52 -0500

    Update PantallaDespacho.java
    llenado a mano de datos en el main

--- boundary/PantallaDespacho.java
+++ boundary/PantallaDespacho.java
@@ -1,7 +1,28 @@
 package boundary;
 
-public class PantallaDespacho {
+import control.*;
+import entities.*;
+import java.util.Calendar;
+import java.util.Scanner;
 
+public class PantallaDespacho {
+	ControlDespachos despacho = new ControlDespachos();
+	Producto arroz = new Producto(4324231, "Arroz", 1800, "Tienda Geminis");
+	Producto lenteja = new Producto(3215432, "Lenteja", 1900, "D1");
+	Cliente ricardo = new Cliente(8908023, "Ricardo Arjona", 4342343, "sfdsfsd");
+	//testeando punto 10
+	despacho.reservarPedido(ricardo.getCedula(), 1113, arroz);
+	despacho.reservarPedido(ricardo.getCedula(), 1114, lenteja);
+	//testeando punto 11
+	despacho.ModificarDatosDeUnPedido(1113);//va en minúscula
+	//testeando punto 12
+	despacho.eliminarReservaDeUnPedido (1113);
+	//testeando punto 13
+	despacho.VerListadoDePedidosExistentes();//minúscula
+	//testeando punto 14
+	despecho.listadoPedidos (1114, new GregorianCalendar(2020,3,30));
+	
+	
 }
 
 /*
