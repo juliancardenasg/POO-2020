81fd68960e01df83db07e40ba74b9bc818979046
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-28 15:07:15 -0500

    Agregue y corregi metodos en la parte de ControlDespachos

--- control/ControlDespachos.java
+++ control/ControlDespachos.java
@@ -1,15 +1,19 @@
 package control;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
-import java.util.Random;
 import entities.Cliente;
 import entities.Producto;
 import entities.Pedido;
+import control.GestionCliente;
+import control.GestionProducto;
+import java.util.Random;
 
 public class ControlDespachos {
 
-  private GestionCliente gestionCliente;
+	 private GestionCliente gestionCliente;
 	 private GestionProducto gestionProducto;
 	 private ArrayList<Cliente> listaClientes;
 	 private ArrayList<Producto> listaProductos;
@@ -81,19 +85,9 @@ public class ControlDespachos {
 		this.pedidos = pedidos;
 	}
 
-  public boolean reservarPedido(long cedula, long pid, Producto producto){
-		if(autoGenerarPedido(cedula, pid) == true) {
-			//PUNTO C ACA
-			if(validacionPedido(cedula, pid) == false) {
-				System.out.println("Nueva fecha de pedido");
-			}
-			else {
-				System.out.println("El costo total de su pedido es ");
-				System.out.println(costoPedido(cedula, pid, producto));
-			}
-			}
 
 
+	public boolean reservarPedido(long cedula, long pid, Producto producto){
 
 	   	return false;
 	}
@@ -126,8 +120,33 @@ public class ControlDespachos {
 	   	return false;
 	}
 
-	public boolean validacionPedido(long cedula, long pid) {
+	public boolean validacionPedido(Pedido pedido) {
 
+		for(Pedido aux : this.pedidos) {
+			if(pedido.getSolicitante().getCedula() == aux.getSolicitante().getCedula()) {
+				if(pedido.getFechaRecibido() == aux.getFechaRecibido()) {
+				System.out.println("Usted debe programar una nueva fecha para su pedido");
+				return false;
+				}
+			}
+			else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
+	public boolean validacionDePedido2 (Pedido pedido) {
+
+		Date fecha = null;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(fecha);
+
+		for(Pedido aux : this.pedidos) {
+			//if(pedido.getFechaRecibido() < fecha.getDate())
+		}
 
 
 		return false;
@@ -141,8 +160,8 @@ public class ControlDespachos {
 
 	public double costoPedido (long cedula, long pid, Producto producto) {
 		double costo = 0;
-		costo = producto.getPrecio + producto.getIva;
-		if(producto.getIva > 50000) {
+		costo = producto.getPrecio() + producto.getIva();
+		if(producto.getIva() > 50000) {
 			costo = costo + 8000;
 		}
 
@@ -151,15 +170,15 @@ public class ControlDespachos {
 
 	public void mostrarPedido (long cedula, long pid, Producto producto, Pedido pedido) {
 
-		if(pedido.getPagado == true) {
+		if(pedido.getPagado() == true) {
 		System.out.println("Confirmación de su pedido");
-		System.out.println("Nª de pedido: " + pedido.getNropedido);
-		System.out.println("Producto: " + producto.getNombre);
-		System.out.println("Precio: " + producto.getPrecio);
-		System.out.println("Precio (IVA): " + producto.getIva);
-		System.out.println("Tienda: " + producto.getTienda);
-		System.out.println("Repartidor: " + pedido.getRepartidor);
-		System.out.println("Fecha de recibido: " + pedido.getFecharecibido);
+		System.out.println("Nª de pedido: " + pedido.getNumeroPedido());
+		System.out.println("Producto: " + producto.getNombreComercial());
+		System.out.println("Precio: " + producto.getPrecio());
+		System.out.println("Precio (IVA): " + producto.getIva());
+		System.out.println("Tienda: " + producto.getTienda());
+		System.out.println("Repartidor: " + pedido.getNombreRepartidor());
+		System.out.println("Fecha de recibido: " + pedido.getFechaRecibido());
 		}
 		else {
 			System.out.println("Por favor haga el pago de su pedido para hacer la confirmacion de su pedido");
@@ -168,16 +187,177 @@ public class ControlDespachos {
 	}
 
 
-	public int listadoPedidos (long pid, Calendar fecha) {
+	//PARTE DE RICHARD
 
+	/*
+	public void ModificarDatosDeUnPedido(long numPedido)
+	{
+	  Scanner sc = new Scanner(System.in);
 
+	  if(existePedido(long numPedido))
+	  {
+	    int opc=0;
+	//System.out.println("");
+	do{
+	  System.out.println("¿QUE DESEA CAMBIAR? \n 1.FECHA DEL PEDIDO \n 2.NOMBRE DEL REPARTIDOR \n3.SERVICIOS ADICIONALES \n4.NADA,SALIR");
+	opc = scanner.nextInt();
+	  switch(opc)
+	  {
+	  	case 1:
+	  	cambiarFecha(numPedido);
+	    break;
+	  	case 2:
+		cambiarNombre(numPedido);
+		break;
+	  	case 3:
+	  	cambiarServicios(numPedido);
+	  	break;
+	  	case 4:
+	  		System.out.println("SE SALIO DE MODIFICAR");
+	  		break;
+	  		default:
+	  		System.out.println("OPCION NO VALIDA");
+	  		break;
+	  }
 
-		return 0;
+	}while(opc!=4);
+	  }
+	  else
+	  System.out.println("No se encontro el pedido");
+	calcularNuevoPrecio(numPedido);
+	}
+	*/
+
+	public void calcularNuevoPrecio(long numPedido)
+	{
+	for(Pedido ped: pedidos)
+	{
+	  if(ped.getNumeroPedido()==numPedido)
+	  {
+	     Scanner sc = new Scanner(System.in);
+	    String op;
+	    System.out.println("SE VA A MODIFICAR EL PRECIO DEL PEDIDO SEGURO DESEA CONTINUAR?(si/no)");
+	    op=sc.next();
+	    if("si".equals(op))
+	    {
+	      /*ped.costoPedido();*/
+	System.out.println("SE MODIFICO EL PRECIO");
+	    }
+	    else
+	    System.out.println("SE MODIFICO EL PRECIO");
+	  }
+	}
 	}
 
+	public void cambiarFecha(long numPedido)
+	{
 
+	  String fecha;
+	  Calendar cal=Calendar.getInstance();
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("INGRESE LA FECHA NUEVA \n EN FORMA: dd-MM-yyyy");
+	  fecha=sc.next();// el String
+	  SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+	  //Date fecha_date = sdf.parse(fecha);
+	  //cal.setTime(fecha_date);// fecha en calendar nueva
+	  for(Pedido ped:pedidos)
+	  {
+	  if(ped.getNumeroPedido()==numPedido)//si es el pedido
+	  {
+		  long temp = ped.getSolicitante().getCedula();//identifico al cliente
+	   Calendar temporal = ped.getFechaRecibido();
+	  		}
+	    //ped.setFechaRecibido(cal);
 
-}
+	   //if(ped.getSolicitante().getCedula() == temp && ped.getNumeroPedido() != numPedido && ped.getFechaRecibido() == temporal)//es el cliente con otro pedido, que tiene la fecha ingresada de cambio
+	   {
+	   System.out.println("YA TIENE OTRO PEDIDO A ESTA FECHA, NO SE LE ASIGANARA");
+	   }
+	   //else
+	   {
+	     System.out.println("SE ASIGNO LA FECHA");
+	    ped.setFechaRecibido(cal);
+	   }
+
+	}
+	}
+
+	public void cambiarNombre(long numPedido)//listo
+	{
+	  String nom;
+	 Scanner sc = new Scanner(System.in);
+	System.out.println("INGRESE EL NOMBRE DEL NUEVO REPARTIDOR") ;
+	nom=sc.next();
+	for(Pedido ped: pedidos)
+	{
+	  if(ped.getNumeroPedido()==numPedido)
+	  ped.setNombreRepartidor(nom);
+	}
+	}
+
+	public void cambiarServicios(long numPedido)
+	{
+	  String servicio;
+	 Scanner sc = new Scanner(System.in);
+	 System.out.println("INGRESE EL SERVICIO");
+	 servicio=sc.next();
+	 //lo dejo por opcional
+	}
+
+	    //12//
+
+	    public boolean eliminarReservaDeUnPedido (long numPedido)
+	    {
+	String confi;
+	      Scanner sc = new Scanner(System.in);
+	          if(existePedido(numPedido)){
+	System.out.println("SE ENCONTRO EL PEDIDO.¿ESTA SEGURO DE QUE QUIERE ELIMINARLO ?(si/no)");
+	confi=sc.next();
+	if("si".equals(confi))
+	{
+	for(Pedido ped: pedidos)
+	{
+	  System.out.println("SE ELIMINO");
+	return this.pedidos.remove(ped);
+	}
+
+	}
+	else
+	System.out.println("NO SE ELIMINO");
+	          }
+	        else
+	System.out.println("NO EXISTE ESTE PEDIDO");
+	      return false;
+	    }
+
+	    //13//
+	    /*
+	    public void VerListadoDePedidosExistentes(){
+	      for(Pedido ped: pedidos )
+	      {
+	        System.out.println("\nCODIGO:"+ped.getNumeroPedido()+"\nFECHA:"+ped.getFechaRecibido()+"\nNOMBRE REPARTIDOR"+ped.getNombreRepartidor()+"\nPRECIO"this.costoPedido(ped.getNumeroPedido()));
+	      }
+	    }
+	    */
+
+	/*
+	public int listadoPedidos (long pid, Calendar fecha) {
+    int cont = 0;
+		Producto auxiliar = null;
+		for(Cliente aux : this.listaClientes) {
+			if(aux.getPedido().getFechaRecibido() == fecha) {
+				auxiliar = aux.getPedido().getProductoSolicitado();
+				if(auxiliar.getPid() == pid) {
+					System.out.println("LISTADO DE PEDIDOS PARA ESTE CLIENTE EN ESTA FECHA");
+					cont++;
+				}
+
+			}
+		}
+		return cont;
+	}
+	*/
+
 
 /*
 Buenas tardes,
