c8f582e51624331c68413c4db434a88a1dfdbf62
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-03-30 07:36:51 -0500

    Version final del entregable uno del proyecto

--- boundary/PantallaDespacho.java
+++ boundary/PantallaDespacho.java
@@ -1,86 +1,206 @@
 package boundary;
-import entities.Pedido;
-import entities.Cliente;
-import entities.Producto;
-import control.ControlDespachos;
-import control.GestionCliente;
-import control.GestionProducto;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Scanner;
 
+import control.*;
+import entities.*;
+import java.util.Calendar;
+import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PantallaDespacho {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+
+		/*ControlDespachos despacho = new ControlDespachos();
+		Producto arroz = new Producto(4324231, "Arroz", 1800, "Tienda Geminis");
+		Producto lenteja = new Producto(3215432, "Lenteja", 1900, "D1");
+		Cliente ricardo = new Cliente(8908023, "Ricardo Arjona", 4342343, "sfdsfsd");
+		//testeando punto 10
+		despacho.reservarPedido(ricardo.getCedula(), 1113, arroz);
+		despacho.reservarPedido(ricardo.getCedula(), 1114, lenteja);
+		//testeando punto 11
+		despacho.ModificarDatosDeUnPedido(1113);//va en minúscula
+		//testeando punto 12
+		despacho.eliminarReservaDeUnPedido (1113);
+		//testeando punto 13
+		despacho.VerListadoDePedidosExistentes();//minúscula
+		//testeando punto 14
+		despacho.listadoPedidos (1114, new GregorianCalendar(2020,3,30));*/
+
+		//Testeando dinámicamente
 
 
-		System.out.println("(1) Ver listado de Productos disponibles");
-		System.out.println("(2) Insertar Productos");
-		System.out.println("(3) Modificar Productos");
-		System.out.println("(4) Eliminar Productos");
-		System.out.println("(5) Ver listado de Clientes registrados en el sistema");
-		System.out.println("(6) Insertar Cliente");
-		System.out.println("(7) Modificar datos de Cliente");
-		System.out.println("(8) Eliminar un Cliente");
-		System.out.println("(9) Realizar el Pedido de un Producto");
-		System.out.println("(A) Modificar un Pedido de un Producto");
-		System.out.println("(B) Eliminar un Pedido de un Producto");
-		System.out.println("(C) Ver listado de Pedidos existentes");
-		System.out.println("(D) Ver listado de Pedidos existentes para un Producto y posterior a una fecha específica");
-		System.out.println("(E) Salir");
 
-        Scanner sc = new Scanner(System.in);
-        char opcion_principal = sc.next().charAt(0);
+		ControlDespachos despacho = new ControlDespachos();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bienvenido al sistema de despacho de productos. \n "
+				+ "Podra realizar las siguientes acciones: \n");
 
-        switch(opcion_principal) {
-        case 1:
-          // code block
-          break;
-        case 2:
-          // code block
-          break;
-        case 3:
-            // code block
-            break;
-        case 4:
-            // code block
-            break;
-        case 5:
-            // code block
-            break;
-        case 6:
-            // code block
-            break;
-        case 7:
-            // code block
-            break;
-        case 8:
-            // code block
-            break;
-        case 9:
-            System.out.println("Bienvenido a el menu para realizar su pedido");
-            break;
-        case 'A':
-            // code block
-            break;
-        case 'B':
-            // code block
-            break;
-        case 'C':
-            // code block
-            break;
-        case 'D':
-            // code block
-            break;
-        case 'E':
-            // code block
-            break;
+		int respuesta;
+		do {
+			System.out.println( "1. Ver listado de Productos disponibles \n"
+					+ "2. Insertar Productos \n"
+					+ "3. Modificar Productos \n"
+					+ "4. Eliminar Productos \n"
+					+ "5. Ver listado de Clientes registrados en el sistema \n"+
+					"6. Insertar Cliente\n" +
+					"7. Modificar datos de Cliente\n" +
+					"8. Eliminar un Cliente\n" +
+					"9. Realizar el Pedido de un Producto\n" +
+					"10. Modificar un Pedido de un Producto\n" +
+					"11. Eliminar un Pedido de un Producto\n" +
+					"12. Ver listado de Pedidos existentes\n" +
+					"13. Ver listado de Pedidos existentes para un Producto y posterior a una fecha específica\n" +
+					"14. Salir \n"
+					);
+
+			respuesta = sc.nextInt();
+
+			switch (respuesta) {
+			case 1:
+				despacho.imprimirProductos();
+				break;
+			case 2:
+
+				System.out.println("Ingrese codigo producto:[7 digitos] ");
+				long pid = sc.nextLong();
+
+				System.out.println("Ingrese nombre del producto: ");
+				String nombreComercial = sc.next();
+
+				System.out.println("Ingrese precio del producto: ");
+				double precio = sc.nextDouble();
+
+				System.out.println("Ingrese tienda");
+				String tienda = sc.next();
+
+				despacho.insertar_un_producto(pid, nombreComercial, precio, tienda);
+				break;
+			case 3:
+				long pid1;
+				do {
+					System.out.println("Ingrese el id del producto a modificar: ");
+					pid1 = sc.nextLong();
+				} while (!despacho.modificar_un_producto(pid1));
+
+				;
+				break;
+			case 4:
+				long pid2;
+				do {
+					System.out.println("Ingrese el id del producto a eliminar: ");
+					pid2 = sc.nextLong();
+				} while (despacho.eliminar_un_producto(pid2));
 
 
-        default:
-          // code block
-      }
+				break;
+			case 5:
+				despacho.verClientes();
+				break;
+			case 6:
+				System.out.println("Ingrese la cedula del cliente: ");
+				long cedula = sc.nextLong();
+
+				System.out.println("Ingrese la nombre del cliente: ");
+				String nombreCompleto = sc.next();
+
+				System.out.println("Ingrese el telefono del cliente: ");
+				long telefonoContacto = sc.nextLong();
+
+				System.out.println("Ingrese la direccion del cliente: ");
+				String direccion = sc.next();
+
+				Cliente ClienteAIngresar = new Cliente(cedula, nombreCompleto, telefonoContacto, direccion);
+				despacho.insertarCliente(ClienteAIngresar);
+				break;
+			case 7:
+				long id1;
+				do {
+					System.out.println("Ingrese el id del producto a modificar: ");
+					id1 = sc.nextLong();
+				} while (!despacho.modificarCliente(id1));
+
+				break;
+			case 8:
+				long id2;
+				do {
+					System.out.println("Ingrese el id del producto a modificar: ");
+					id2 = sc.nextLong();
+				} while (!despacho.eliminarCliente(id2));
+				break;
+			case 9:
+				System.out.println("----------------------Datos del producto---------------");
+				System.out.println("Ingrese codigo producto:[7 digitos] ");
+				long pid3 = sc.nextLong();
+
+				System.out.println("Ingrese nombre del producto: ");
+				String nombreComercial3 = sc.next();
+
+				System.out.println("Ingrese precio del producto: ");
+				double precio3 = sc.nextDouble();
+
+				System.out.println("Ingrese tienda");
+				String tienda3 = sc.next();
+				Producto productoPedido = new Producto(pid3, nombreComercial3,precio3,tienda3);
+				System.out.println("----------------------Datos del Cliente---------------");
+				System.out.println("Ingrese la cedula del cliente: ");
+				long cedula2 = sc.nextLong();
+
+				System.out.println("Ingrese la nombre del cliente: ");
+				String nombreCompleto2 = sc.next();
+
+				System.out.println("Ingrese el telefono del cliente: ");
+				long telefonoContacto2 = sc.nextLong();
+
+				System.out.println("Ingrese la direccion del cliente: ");
+				String direccion2 = sc.next();
+
+				Cliente clientePedido= new Cliente (cedula2, nombreCompleto2, telefonoContacto2, direccion2);
+				System.out.println("Ingrese el id del pedido: ");
+				long id_pedido = sc.nextLong();
+				despacho.reservarPedido(clientePedido.getCedula(), id_pedido, productoPedido);
+				break;
+			case 10:
+				System.out.println("Ingrese el id del pedido a modificar: ");
+				long id_pedido2 = sc.nextLong();
+				despacho.ModificarDatosDeUnPedido(id_pedido2);//va en minúscula
+				break;
+			case 11:
+				System.out.println("Ingrese el id del pedido a eliminar: ");
+				long id_pedido3 = sc.nextLong();
+				despacho.eliminarReservaDeUnPedido (id_pedido3);
+				break;
+			case 12:
+				despacho.VerListadoDePedidosExistentes();
+				break;
+			case 13:
+				System.out.println("Ingrese el codigo del producto: ");
+				long pid4 = sc.nextLong();
+
+				String fecha;
+		        Calendar cal = Calendar.getInstance();
+		        System.out.println("INGRESE LA FECHA NUEVA \n EN FORMA: dd-MM-yyyy");
+		        fecha = sc.next();// el String
+		        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		        Date fecha_date = sdf.parse(fecha);
+
+		        cal.setTime(fecha_date);
+		        //System.out.println(cal.get(Calendar.DAY_OF_MONTH)+"-"+cal.get(Calendar.MONTH+1)+"-"+cal.get(Calendar.YEAR));
+
+				despacho.verListadodePedidosParaProductoyFecha(pid4, cal);
+				break;
+			case 14:
+				System.out.println("Gracias por usar el sistema de despacho");
+				break;
+			default:
+				break;
+			}
+
+		} while (respuesta != 14);
+
+
+
 
 	}
-	}
+}
--- entities/Cliente.java
+++ entities/Cliente.java
@@ -1,5 +1,53 @@
 package entities;
 
 public class Cliente {
+	//----------------Attributes------------------------
+	private long cedula;
+	private String nombreCompleto;
+	private long telefonoContacto;
+	private String direccion;
+	//----------------Constructors------------------------
+	public Cliente(long cedula, String nombreCompleto, long telefonoContacto, String direccion) {
+		this.cedula = cedula;
+		this.nombreCompleto = nombreCompleto;
+		this.telefonoContacto = telefonoContacto;
+		this.direccion = direccion;
+	}
+	public Cliente(){}
 
+	//----------------Accessors------------------------
+	public long getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(long cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombreCompleto() {
+		return nombreCompleto;
+	}
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}
+	public long getTelefonoContacto() {
+		return telefonoContacto;
+	}
+	public void setTelefonoContacto(long telefonoContacto) {
+		this.telefonoContacto = telefonoContacto;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	//----------------Methods------------------------
+	@Override
+	public String toString() {
+		return "Cedula: " + this.cedula +"; \n" +
+			   "1.Nombre completo: " + this.nombreCompleto+"; \n" +
+			   "2.Telefono de contacto: " + this.telefonoContacto + "; \n" +
+			   "3.Dirección: " + this.direccion + "; \n";
+	}
 }
