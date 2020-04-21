e821fec6d0ec17f31dde4f7b06c258c2f90a5262
Julian Cardenas <cardenas.julian1031@gmail.com>
2020-04-21 00:55:16 -0500

    Cambios y mas cambios

---
+++ entities/BonoRegalo.java
@@ -0,0 +1,53 @@
+package entities;
+
+import java.util.Date;
+
+public class BonoRegalo extends ServicioAdicional {
+	
+	public String comercioAsociado;
+	public String mensaje;
+	public Date fechaVencimiento;
+
+	public BonoRegalo() {
+		super();
+	}
+
+	public BonoRegalo(String comercioAsociado, String mensaje, Date fechaVencimiento) {
+		super();
+		this.comercioAsociado = comercioAsociado;
+		this.mensaje = mensaje;
+		this.fechaVencimiento = fechaVencimiento;
+	}
+	
+	public String getComercioAsociado() {
+		return comercioAsociado;
+	}
+
+	public void setComercioAsociado(String comercioAsociado) {
+		this.comercioAsociado = comercioAsociado;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public Date getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+
+	public void setFechaVencimiento(Date fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+
+	@Override
+	public Double calcularPrecio() {
+		this.precio = this.precio;
+		return null;
+	}
+
+	
+}
---
+++ entities/TipoTransporte.java
@@ -0,0 +1,9 @@
+package entities;
+
+public enum TipoTransporte {
+	
+	BICICLETA,
+	MOTO,
+	MINIVAN,
+
+}
